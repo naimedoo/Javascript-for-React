ca2f7f642883d28a4f77f9b75bdf477b96ad4877
naim edoo <naim.e2703@gmail.com>
2022-02-21 15:33:15 +0100

    response exercise-1

---
+++ 01-exercice/consignes.js
@@ -0,0 +1,18 @@
+/**
+ * Convertir des pouces en centimetres
+ * Vous pouvez utiliser les arguments depuis le terminal ou avec prompt.
+ * L'utilisateur saisit un nombre de pouces à convertir
+ * 1" (1 pouce) = 2,54 centimètres  
+ * 
+ * https://www.pouce-cm.fr/
+ */
+
+
+// Write your code here
+
+let prompt = require('prompt');
+prompt.start();
+prompt.get("pouces" ,function(err,result) {
+   console.log(result.pouces * 2.54)
+  
+})
