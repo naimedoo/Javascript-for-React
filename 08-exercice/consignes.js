fc549670c0f5266fd18663f026ea897edb8c709e
naim edoo <naim.e2703@gmail.com>
2022-02-21 17:18:53 +0100

    response exercise-8

---
+++ 08-exercice/consignes.js
@@ -0,0 +1,9 @@
+/**
+ *  Faire une fonction qui affiche la soustraction de 2 nombres
+ */
+
+// Write your code here
+function substraction(num1, num2) {
+    console.log(num1 - num2)
+}
+substraction(50, 5)
