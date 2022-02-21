5984f82644e1d265ec5290d3e4d0ad582d7360a8
naim edoo <naim.e2703@gmail.com>
2022-02-21 16:42:29 +0100

    response exercise-4

---
+++ 04-exercice/consignes.js
@@ -0,0 +1,10 @@
+/**
+ * Faire une fonction qui prend un paramètre 'prenom' et affiche Bonjour suivi du prénom
+ */
+
+// Write your code here
+
+function name(prenom) {
+    console.log("bonjour" + " " + prenom)
+}
+name('Naïm')
