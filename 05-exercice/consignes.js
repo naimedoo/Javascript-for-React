f6884192a8773ee735667c94f93fed5ee1631604
naim edoo <naim.e2703@gmail.com>
2022-02-21 17:00:04 +0100

    response exercise-5

---
+++ 05-exercice/consignes.js
@@ -0,0 +1,13 @@
+/** 
+  * Faire une fonction qui affiche la table de multiplication d'un nombre passé en paramètre
+*/
+// Write your code here
+
+function multiply(num) {
+  for (let i = 1; i <= 10; i++) {
+       
+       console.log(num * i)
+   }
+
+}
+multiply(3)
