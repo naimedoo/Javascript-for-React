4b9570546ff6d70bdf18ac8548f0531e52d8d186
naim edoo <naim.e2703@gmail.com>
2022-02-22 09:19:00 +0100

    response exercise-10

---
+++ 10-exercice/consignes.js
@@ -0,0 +1,11 @@
+/**
+ *  Faire une fonction anonyme qui affiche la soustraction de 2 nombres
+ */
+
+// Write your code here
+
+
+let substraction =  function (num1,num2) {
+    console.log(num1 - num2)
+    }
+    substraction(75, 30)
