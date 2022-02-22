5cf95644664e0b90eb57bdf8b1a91fdc45b144e1
naim edoo <naim.e2703@gmail.com>
2022-02-22 09:26:27 +0100

    response exercise-12

---
+++ 12-exercice/consignes.js
@@ -0,0 +1,9 @@
+/**
+ * A l'aide de la méthode foreach, parcourir le tableau et afficher les langages en majuscule.
+ */
+let languages = ["JavaScript", "HTML",  "CSS", "nodejs"];
+
+ // Write your code here.
+ languages.forEach(function(language){
+     console.log(language.toUpperCase())
+ })
